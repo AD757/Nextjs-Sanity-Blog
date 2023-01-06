90883f0a15615d89bc988f2884e0049e4c6c0624
ad757 <alladin.daher@hotmail.com>
2023-01-06 09:30:10 +0100

    Destructed code

---
+++ components/PostCard.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Image from "next/image";
+import urlFor from "../lib/urlFor";
+import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
+
+interface Props {
+  post: Post;
+}
+
+const PostCard = ({ post }: Props) => {
+  return (
+    <div className="flex flex-col group cursor-pointer">
+      <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+        <Image
+          className="object-cover object-left lg:object-center rounded-lg"
+          src={urlFor(post.mainImage).url()}
+          alt={post.author.name}
+          fill
+        />
+        <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
+          <div>
+            <p className="font-bold">{post.title}</p>
+            <p>
+              {new Date(post._createdAt).toLocaleDateString("en-US", {
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+              })}
+            </p>
+          </div>
+
+          <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+            {post.categories.map((category) => (
+              <div className="bg-[#8F00FF] text-center text-white px-3 py-1 rounded-lg text-sm font-semibold]">
+                <p>{category.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-5 flex-1">
+        <p className="underline text-lg font-bold">{post.title}</p>
+        <p className="line-clamp-2 text-gray-500">{post.description}</p>
+      </div>
+
+      <p className="mt-5 font-bold flex items-center group-hover:underline">
+        Read Post
+        <ArrowUpRightIcon className="ml-2 h-4 w-4" />
+      </p>
+    </div>
+  );
+};
+
+export default PostCard;
